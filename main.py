98bf756fa63c624350aaf69335526a73fc9e62fc
Patrick D <patrick.montejo@gmail.com>
2024-01-27 12:47:52 -0500

    added python data type samples

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-
-
 print("This is a test or practice for Github ")
+
+# this is a comment
+
+a_variable = "this is a variable"  # assigned to a variable
+a_number = 1  # this is an int
+a_float = 1.00  # this is a float
+yes_no = True  # this is a boolean
+
+# assigning data type
+
+sample1 = str(1)  # string
+sample2 = int(1)  # int
+sample3 = float(1)  # float
+
+# if you want to know the data type
+
+print(type(sample1))  # result <class 'str'> string
+
+
